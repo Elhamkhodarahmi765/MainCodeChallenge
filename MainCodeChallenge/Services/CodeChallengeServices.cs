2ebe821d7376c9e577d022b6c3ed8dd566be0815
ElhamKhodarahmi <khodarahmielhame@gmail.com>
2023-03-05 18:53:39 +0330

    DB

--- MainCodeChallenge/Services/CodeChallengeServices.cs
+++ MainCodeChallenge/Services/CodeChallengeServices.cs
@@ -595,6 +595,7 @@ namespace MainCodeChallenge.Services
                     }
                 }else if (result.SQStatus == (int)EnumSQStatus.Done)
                 {
+
                     Tbl_ApprovalStatus Aps = new Tbl_ApprovalStatus();
                     Aps.SQId = Qid;
                     Aps.SQPid = PUid;
